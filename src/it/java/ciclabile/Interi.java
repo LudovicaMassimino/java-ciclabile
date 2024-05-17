5b429ed15b092811d9cb1ad17e1289b6e2395326
LudovicaMassimino <ludovica.massimino93@gmail.com>
2024-05-17 19:06:44 +0200

    funziona

---
+++ src/it/java/ciclabile/Interi.java
@@ -0,0 +1,72 @@
+/* Consegna:
+ * Creare una classe che gestisce un elenco di interi e permette 
+ * di ciclare (for) attraverso di essi con i metodi:
+ * - getElementoSuccessivo che restiruisce l'elemento corrente e 
+ * incrementa
+ * - hasAncoraElementi che deve restituire true se ci sono ancora 
+ * elementi da restituire
+ * 
+ * Bonus: prevedere anche un costruttore che non prenda parametri 
+ * e un metodo addElemento che permetta di aggiungere un nuovo 
+ * intero all’elenco da ciclare. */
+
+package it.java.ciclabile;
+
+public class Interi {
+	
+	private int[]elementi;
+	private int indiceCorrente;
+	private int numeroElementi;
+	
+	public int getNumeroElementi() {
+		return numeroElementi;
+	}
+
+	// costruttore che accetta un array di interi e lo assegna all'attributo elementi
+	public Interi(int[] elementiIniziali) {
+		this.elementi = elementiIniziali;
+		this.numeroElementi = elementi.length;
+		this.indiceCorrente = 0;
+	}
+	
+	// costruttore per inizializzare un array vuoto
+	public Interi() {
+		this.elementi = new int[10];
+		this.numeroElementi = 0;
+		this.indiceCorrente = 0;
+	}
+
+	
+	// Metodo per aggiungere un nuovo elemento all'elenco
+    public void addElemento(int nuovoElemento) {
+        if (numeroElementi == elementi.length) {
+            // Espande l'array se necessario
+            int[] nuovoArray = new int[elementi.length * 2];
+            for (int i = 0; i < elementi.length; i++) {
+                nuovoArray[i] = elementi[i];
+            }
+            elementi = nuovoArray;
+        }
+        elementi[numeroElementi] = nuovoElemento;
+        numeroElementi++;
+    }
+    
+    // Restituisce l'elemento corrente e incrementa
+    public int getElementoSuccessivo() {
+        int elemento = elementi[indiceCorrente];
+        indiceCorrente++;
+        return elemento;
+    }
+
+    // Restituisce true se ci sono ancora elementi da restituire
+    public boolean hasAncoraElementi() {
+        return indiceCorrente < numeroElementi;
+    }
+    
+    public void stampa() {
+        if (hasAncoraElementi()) {
+            System.out.println(getElementoSuccessivo());
+            stampa();
+        }
+    }
+}
---
+++ src/it/java/ciclabile/Main.java
@@ -0,0 +1,25 @@
+package it.java.ciclabile;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// inizializzazione con un array di interi
+		int[] numeri = {1, 2, 3, 4, 5};
+		Interi myInteri = new Interi(numeri);
+		
+		// Uso dei metodi getElementoSuccessivo e hasAncoraElementi
+        myInteri.stampa();
+
+        // Creazione di un'istanza senza parametri e aggiunta di elementi
+        Interi elenco = new Interi();
+        elenco.addElemento(10);
+        elenco.addElemento(15);
+        elenco.addElemento(20);	
+	
+        // riportare la posizione a 0 per ciclarli ancora
+		elenco.getNumeroElementi();
+		
+		// Stampa tutti gli elementi nuovamente utilizzando la ricorsione
+        elenco.stampa();
+    }
+}
